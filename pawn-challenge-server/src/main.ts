d549f64efc3a9abd6f8cbfe64a8a67be365f50de
unknown <hsu.tranhuynhduc@gmail.com>
2022-08-29 10:41:17 +0700

    new to gitignore

--- pawn-challenge-server/src/main.ts
+++ pawn-challenge-server/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as admin from 'firebase-admin'
+import * as env from 'environment'
 
 async function bootstrap() {
   admin.initializeApp({
-    credential: admin.credential.cert('key-admin.json')
+    credential: admin.credential.cert(env.environment.connectionString)
   });
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
