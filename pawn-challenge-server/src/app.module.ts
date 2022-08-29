c5ee7bbb0f90c078f1e6eeece98c09a28f64d54f
KhoaNA <khoa.na1266@sinhvien.hoasen.edu.vn>
2022-08-29 11:29:35 +0700

    pull

--- pawn-challenge-server/src/app.module.ts
+++ pawn-challenge-server/src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './message/controller/user/user.controller';
 import { MessageModule } from './message/message.module';
+import { UserService } from './message/service/user/user.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb+srv://admin:admin@cluster0.ey0prhj.mongodb.net/?retryWrites=true&w=majority"),
     MessageModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
---
+++ pawn-challenge-server/src/message/controller/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from 'src/message/schemas/user.shema';
+import { UserService } from 'src/message/service/user/user.service';
+@Controller('user')
+export class UserController {
+    constructor(private UserService: UserService) {}
+
+    @Post('/')
+    public async createUser(@Body() newUser: User) {
+        return await this.UserService.createUser(newUser);
+    }
+    
+}
